ad896b8679a8bb60935b867a0169b00537414a92
Elliott <85990359+Ell1ott@users.noreply.github.com>
2023-10-30 20:45:49 +0100

    fix yarn buil derror

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import Layout from "../components/Layout";
 import BasicMeta from "../components/meta/BasicMeta";
 import OpenGraphMeta from "../components/meta/OpenGraphMeta";
 import TwitterCardMeta from "../components/meta/TwitterCardMeta";
-import Navbar from "../components/Navbar";
 import { SocialList } from "../components/SocialList";
 
 export default function Index() {
