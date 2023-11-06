2b37d20b21492e9d21b868c723a6d18543c1068f
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2023-11-06 12:31:01 +0100

    change some margins

--- src/components/HSeparator.tsx
+++ src/components/HSeparator.tsx
@@ -8,7 +8,7 @@ export default function HSeparator() {
             height: 2px;
             width: 100%;
             background-color: rgba(0, 0, 0, 0.658);
-            margin: 0.5rem 0;
+            margin: 0rem 0;
           }
         `}
       </style>
