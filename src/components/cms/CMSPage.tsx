e831baff384714c26e22a89f3338d9551425f8aa
Jonathan Bangert <jonathan@bangert.dk>
2023-11-06 21:06:26 +0100

    Add preview template

--- src/components/cms/CMSPage.tsx
+++ src/components/cms/CMSPage.tsx
@@ -40,6 +40,19 @@ const CMSPage: FC = () => {
     };
     CMS.registerWidget("slug", SlugControl);
 
+    CMS.registerPreviewStyle(
+        "https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400&display=swap"
+    );
+    var PostPreview = ({ entry }) => {
+      return(
+        <div className="content">
+          <h1>{entry.data.title}</h1>
+          <time>{entry.data.date}</time>
+        </div>
+      )
+    };
+    CMS.registerPreviewTemplate("posts", PostPreview);
+
     CMS.init({ config });
   }, []);
 
