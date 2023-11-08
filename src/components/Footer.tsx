8a3d9196828a8f978fbf98960728779137550969
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2023-11-08 10:46:50 +0100

    change footer to blured version of bg img

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,9 +8,21 @@ export default function Footer() {
         {`
 
           .container{
-            background-color: blue;
+            background-image: url("/images/chess-bg.jpg");
+            background-size: cover;
+            background-position: bottom;
             height: 12rem;
             width: 100%;
+            overflow: hidden;
+          }
+
+          .container::after {
+            content: "";
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            backdrop-filter: blur(10px); /* apply the blur */
+            pointer-events: none; /* make the overlay click-through */
           }
           p {
             font-size: 0.75rem;
