147bd1b65cfb580a0085ec8ee2b06329dd8f7f80
Jonathan Bangert <jonathan@bangert.dk>
2023-11-17 15:20:46 +0100

    Fix css-in-js in app router and update dependencies

--- next.config.js
+++ next.config.js
@@ -24,5 +24,5 @@ module.exports = withBundleAnalyzer({
   },
   images: {
     unoptimized: true,
-  }
+  },
 });
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 // NOTE: Do not move the styles dir to the src.
 // They are used by the Netlify CMS preview feature.
 import "../../public/styles/global.css";
-
+import StyledComponentsRegistry from "./registry";
+ 
 export const metadata = {
   title: 'Nørrebro skakklub',
   description: 'Nørrebro skakklub hjemmeside',
@@ -18,7 +19,9 @@ export default function RootLayout({
             <link rel="apple-touch-startup-image" href="ios-startup.png" />
             <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         </head>
-        <body>{children}</body>
+        <body>
+          <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        </body>
     </html>
   )
 }
