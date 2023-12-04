0df50f0a29a33a295b99c85d5c85c04339383ea8
Elliott <85990359+Ell1ott@users.noreply.github.com>
2023-12-04 19:15:00 +0100

    use css modules for 404 not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,26 +1,13 @@
-'use client';
 import { Button } from "@mui/joy";
 import Link from "next/link";
+import styles from './not-found.module.scss';
 
 export default function NotFound() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <h2>404 - Trækmisforståelse!</h2>
       <p>Uh-oh! Det ser ud til, at vi er gået glip af det træk. Denne side er vist et skakmat for forbindelsen!</p>
-      <Link href='/'><Button size='lg' sx={{ marginTop: 2, backgroundColor: 'orange', ':hover': { backgroundColor: 'rgb(255, 140, 0)' } }} >Gå tilbage</Button></Link>
-      <style jsx>
-        {`
-          .container {
-            flex-direction: column;
-            height: 100%;
-            width: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-wrap: wrap;
-          }
-        `}
-      </style>
-    </div>
+      <Link href='/'><Button size='lg' className={styles.button} >Gå tilbage</Button></Link>
+    </div >
   );
 }
