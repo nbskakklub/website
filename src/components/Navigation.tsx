c1c8aa9e7e56baf0cbf542898a42851c0a3f88a6
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2023-12-05 19:20:33 +0100

    fix navbar not being visible enough

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,102 +1,91 @@
-'use client'
+"use client";
 
 import Link from "next/link";
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 import Burger from "./Burger";
 import { useState } from "react";
 
-
 function activeClassname(pathName, path: string, subAllowed = true) {
   if (subAllowed) {
-    return path.startsWith("/" + pathName) ? "active" : undefined
+    return path.startsWith("/" + pathName) ? "active" : undefined;
   } else {
-    return path.endsWith("/" + pathName) ? "active" : undefined
+    return path.endsWith("/" + pathName) ? "active" : undefined;
   }
 }
 export default function Navigation() {
   var pathname = usePathname();
-  if (pathname == null) { pathname = '' }
+  if (pathname == null) {
+    pathname = "";
+  }
   const [active, setActive] = useState(false);
-  return <>
-    <Burger active={active} onClick={() => setActive(!active)} />
-    <div className={"container " + (active ? "active" : "")}>
-      <ul>
-        <li>
-          <Link href="/" legacyBehavior>
-            <a className={activeClassname("", pathname, false)}>
-              Om Nørrebro Skakklub
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/calendar" legacyBehavior>
-            <a
-              className={
-                activeClassname("calendar", pathname)
-              }
-            >
-              Kalender
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/posts" legacyBehavior>
-            <a
-              className={
-                activeClassname("posts", pathname)
-              }
-            >
-              Nyheder
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/hall-of-fame" legacyBehavior>
-            <a
-              className={
-                activeClassname("hall-of-fame", pathname)
-              }
-            >
-              Hall of Fame
-            </a>
-          </Link>
-        </li>
-      </ul>
-      <style jsx>
-        {`
-          ul {
+  return (
+    <>
+      <Burger active={active} onClick={() => setActive(!active)} />
+      <div className={"container " + (active ? "active" : "")}>
+        <ul>
+          <li>
+            <Link href="/" legacyBehavior>
+              <a className={activeClassname("", pathname, false)}>
+                Om Nørrebro Skakklub
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/calendar" legacyBehavior>
+              <a className={activeClassname("calendar", pathname)}>Kalender</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/posts" legacyBehavior>
+              <a className={activeClassname("posts", pathname)}>Nyheder</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/hall-of-fame" legacyBehavior>
+              <a className={activeClassname("hall-of-fame", pathname)}>
+                Hall of Fame
+              </a>
+            </Link>
+          </li>
+        </ul>
+        <style jsx>
+          {`
+            .container {
+              margin-left: 7rem;
+            }
+            ul {
               display: flex;
               flex-direction: row;
               list-style: none;
               gap: 30px;
-              justify-content: center;
+              justify-content: start;
               align-items: center;
-          }
-          .active ul {
-            opacity: 1;
-          }
-          li {
-            font-size: 2rem;
-            
-          }
-          .active {
-            color: #222;
-          }
-
-          @media (max-width: 769px) {
-            .container {
-              display: block;
             }
-            ul {
+            .active ul {
               opacity: 1;
-              padding: 0;
             }
             li {
-              font-size: 1rem;
+              font-size: 1.2rem;
             }
-          }
-        `}
-      </style>
-    </div>
-  </>;
+            .active {
+              color: #222;
+            }
+
+            @media (max-width: 769px) {
+              .container {
+                display: block;
+              }
+              ul {
+                opacity: 1;
+                padding: 0;
+              }
+              li {
+                font-size: 1rem;
+              }
+            }
+          `}
+        </style>
+      </div>
+    </>
+  );
 }
