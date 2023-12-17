8b89336cd5a5fba90ac3946b719465f60e75923c
Elliott <85990359+Ell1ott@users.noreply.github.com>
2023-12-17 15:55:11 +0100

    create a better footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,14 +6,46 @@ export default function Footer() {
     <>
       <div className={styles.container}>
         <div className={styles.content}>
+          <div className={styles.om}>
+            <strong><p>Om Nørrebro skakklub</p></strong>
+            <p>Med godt og vel 60 medlemmer er Nørrebro en af de største skakklubber i København, tillige har vi en stor juniorafdeling.Nørrebro Skakklub blev grundlagt i januar 2013</p>
+          </div>
+          <div>
+            <strong><p>Adresse</p></strong>
+            <p>Frejasgade 14</p>
+            <p>2200 København</p>
+          </div>
+          <div>
+            <strong><p>Kontakt</p></strong>
+            <p>Kim Secher Andersen</p>
+
+            <a href="mailto:kimsecher@gmail.com">
+              <p>kimsecher@gmail.com</p>
+            </a>
+          </div>
+          <div>
+            <strong><p>Info</p></strong>
+            <a href="/">
+              <p>Om Nørrebro Skakklub</p>
+            </a>
+            <a href="/calendar">
+              <p>Kalender</p>
+            </a>
+            <a href="/posts">
+              <p>Nyheder</p>
+            </a>
+            <a href="/hall-of-fame">
+              <p>Hall of Fame</p>
+            </a>
+          </div>
           <Link href="/admin">Admin</Link>
-        </div>
-        <hr />
-        <div className={styles.contentBottom}>
-          <p>Copyright © 2023 Nørrebro Skakklub</p>|
-          <a href="https://example.com/" className="underline-effect">
-            Created by TSDT
-          </a>
+          <hr />
+          <div className={styles.contentBottom}>
+            <p>Copyright © 2023 Nørrebro Skakklub</p>|
+            <a href="https://example.com/" className={styles.underlineEffect}>
+              Created by TSDT
+            </a>
+          </div>
         </div>
       </div>
     </>
