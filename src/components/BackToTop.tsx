abbc8cf750cfd2bc09ad499277699424b2d7e1b8
Jonathan Bangert <jonathan@bangert.dk>
2023-12-17 17:52:34 +0100

    Use html thing instead of js

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -1,22 +1,11 @@
 "use client"
 
-import { ReactElement } from "react";
-import style from "styled-jsx/style";
-
-function scrollToTop() {
-    window.scroll({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
-    });
-}
-
 export default function BackToTop(
     { children }: { children: React.ReactNode }
 ) {
     return (
         <>
-            <a onClick={scrollToTop}>{children}</a>
+            <a href="#top">{children}</a>
             <style jsx>
                 {`
                 a {
