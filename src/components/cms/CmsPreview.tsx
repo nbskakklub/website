f794b7ce3a07f42115a1d5e07c81d97bb14d27a8
Jonathan Bangert <jonathan@bangert.dk>
2023-12-25 22:34:25 +0100

    Add footer to cms preview

--- src/components/cms/CmsPreview.tsx
+++ src/components/cms/CmsPreview.tsx
@@ -94,14 +94,47 @@ export default function Page({ title, cards, children }) {
             <footer>
                 <div className="footer_container">
                     <div className="content">
-                    <a>Admin</a>
+                    <div className="om">
+                        <strong><p>Om Nørrebro skakklub</p></strong>
+                        <p>Med godt og vel 60 medlemmer er Nørrebro en af de største skakklubber i København, tillige har vi en stor juniorafdeling.Nørrebro Skakklub blev grundlagt i januar 2013</p>
                     </div>
-                    <hr />
-                    <div className="contentBottom">
-                    <p>Copyright © 2023 Nørrebro Skakklub</p>|
-                    <a href="https://example.com/" className="underline-effect">
-                        Created by TSDT
+                    <div>
+                        <strong><p>Adresse</p></strong>
+                        <div className="adress">
+                        <p>Frejasgade 14</p>
+                        <p>2200 København</p>
+                        </div>
+                    </div>
+                    <div>
+                        <strong><p>Kontakt</p></strong>
+                        <p>Kim Secher Andersen</p>
+
+                        <a href="mailto:kimsecher@gmail.com">
+                        <p>kimsecher@gmail.com</p>
+                        </a>
+                    </div>
+                    <div>
+                        <strong><p>Info</p></strong>
+                        <a href="/">
+                        <p>Om Nørrebro Skakklub</p>
+                        </a>
+                        <a href="/calendar">
+                        <p>Kalender</p>
+                        </a>
+                        <a href="/posts">
+                        <p>Nyheder</p>
+                        </a>
+                        <a href="/hall-of-fame">
+                        <p>Hall of Fame</p>
+                        </a>
+                    </div>
+                    <p className="copyright">Copyright © 2023 Nørrebro Skakklub</p>
+
+                    <a href="https://example.com/" >
+                        <p className="underlineEffect">Created by TSDT</p>
                     </a>
+                    <a href="/admin"><p>Admin</p></a>
+                    <a href="#top"><p>Til toppen ↑</p></a>
                     </div>
                 </div>
             </footer>
