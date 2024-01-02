70468a0ee6371440d6929ee1149c776c1ca0ebaf
Jonathan Bangert <jonathan@bangert.dk>
2024-01-02 13:03:59 +0100

    Improve and fix calendar layout

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,26 +1,26 @@
-'use client'
+"use client";
 
-import React, { Fragment, useState } from 'react'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import googleCalendarPlugin from '@fullcalendar/google-calendar';
-import daLocale from '@fullcalendar/core/locales/da';
-import listPlugin from '@fullcalendar/list';
-import ModalOverflow from '@mui/joy/ModalOverflow';
-import ModalDialog from '@mui/joy/ModalDialog';
-import { Modal, ModalClose, Typography } from '@mui/joy';
-import parse from 'html-react-parser'
+import React, { Fragment, useState } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import googleCalendarPlugin from "@fullcalendar/google-calendar";
+import daLocale from "@fullcalendar/core/locales/da";
+import listPlugin from "@fullcalendar/list";
+import ModalOverflow from "@mui/joy/ModalOverflow";
+import ModalDialog from "@mui/joy/ModalDialog";
+import { Modal, ModalClose, Typography } from "@mui/joy";
+import parse from "html-react-parser";
 
 type Props = {
   googleCalendarId: string;
 };
 export default function Calendar({ googleCalendarId }: Props) {
   const [modalOpen, setModalOpen] = useState(false);
-  const [modalContent, setModalContent] = useState('');
-  const [modalTitle, setModalTitle] = useState('');
-  const [modalLocation, setModalLocation] = useState('');
-  const [modalStartDate, setModalStartDate] = useState(new Date);
-  const [modalEndDate, setModalEndDate] = useState(new Date);
+  const [modalContent, setModalContent] = useState("");
+  const [modalTitle, setModalTitle] = useState("");
+  const [modalLocation, setModalLocation] = useState("");
+  const [modalStartDate, setModalStartDate] = useState(new Date());
+  const [modalEndDate, setModalEndDate] = useState(new Date());
 
   return (
     <>
@@ -29,16 +29,21 @@ export default function Calendar({ googleCalendarId }: Props) {
         aria-describedby="modal-desc"
         open={modalOpen}
         onClose={() => setModalOpen(false)}
-        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', maxWidth:'100%' }}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          maxWidth: "100%",
+        }}
       >
         <ModalOverflow>
           <ModalDialog
-            layout='center'
+            layout="center"
             sx={{
               maxWidth: 500,
-              borderRadius: 'md',
+              borderRadius: "md",
               p: 3,
-              boxShadow: 'lg',
+              boxShadow: "lg",
             }}
           >
             <div>
@@ -54,112 +59,122 @@ export default function Calendar({ googleCalendarId }: Props) {
               >
                 {modalTitle}
               </Typography>
-              <Typography sx={{ marginBottom: 2 }} >{Intl.DateTimeFormat('da', {
-                  dateStyle: 'full',
-                  timeStyle: 'short',
-                  timeZone: 'Europe/Copenhagen',
-                }).format(modalStartDate)} - {Intl.DateTimeFormat('da', {
-                  dateStyle: 'full',
-                  timeStyle: 'short',
-                  timeZone: 'Europe/Copenhagen',
-                }).format(modalEndDate)}</Typography>
+              <Typography sx={{ marginBottom: 2 }}>
+                {Intl.DateTimeFormat("da", {
+                  dateStyle: "full",
+                  timeStyle: "short",
+                  timeZone: "Europe/Copenhagen",
+                }).format(modalStartDate)}{" "}
+                -{" "}
+                {Intl.DateTimeFormat("da", {
+                  dateStyle: "full",
+                  timeStyle: "short",
+                  timeZone: "Europe/Copenhagen",
+                }).format(modalEndDate)}
+              </Typography>
             </div>
             {modalLocation ? (
               <iframe
                 width="100%"
                 height="300"
                 loading="lazy"
-                src={"https://www.google.com/maps/embed/v1/place?key=AIzaSyBOhX-3VPGdJkhaX7IqD60Gh71V898AtcY&q="+modalLocation}>
-              </iframe>
+                src={
+                  "https://www.google.com/maps/embed/v1/place?key=AIzaSyBOhX-3VPGdJkhaX7IqD60Gh71V898AtcY&q=" +
+                  modalLocation
+                }
+              ></iframe>
             ) : null}
-            <Typography id="modal-desc" textColor="inherit" >{parse(modalContent)}</Typography>
+            <Typography id="modal-desc" textColor="inherit">
+              {parse(modalContent)}
+            </Typography>
           </ModalDialog>
         </ModalOverflow>
       </Modal>
       <FullCalendar
-        plugins={[ dayGridPlugin, googleCalendarPlugin, listPlugin ]}
-        googleCalendarApiKey='AIzaSyBOhX-3VPGdJkhaX7IqD60Gh71V898AtcY'
-        eventSources={[
-          { googleCalendarId: googleCalendarId }
-        ]}
+        plugins={[dayGridPlugin, googleCalendarPlugin, listPlugin]}
+        googleCalendarApiKey="AIzaSyBOhX-3VPGdJkhaX7IqD60Gh71V898AtcY"
+        eventSources={[{ googleCalendarId: googleCalendarId }]}
         locales={[daLocale]}
-        customButtons={
-          {subscribeButton: {
-            text: 'Abonner på kalender',
-            click: function() {
-              window.open(`https://calendar.google.com/calendar/u/0/r?cid=${ googleCalendarId }`, '_blank');
+        customButtons={{
+          subscribeButton: {
+            text: "Abonner på kalender",
+            click: function () {
+              window.open(
+                `https://calendar.google.com/calendar/u/0/r?cid=${googleCalendarId}`,
+                "_blank"
+              );
             },
             hint: "Abonner på kalenderen",
-          }
+          },
         }}
-        initialView='dayGridMonth'
+        initialView="dayGridMonth"
         headerToolbar={{
-          center: 'prev,today,next',
-          right: 'subscribeButton'
+          center: "prev,today,next",
+          right: "subscribeButton",
         }}
-        windowResize={function() {
+        windowResize={function () {
           if (window.innerWidth < 767) {
-            this.changeView('listMonth');
-            this.setOption('headerToolbar', {
-              left: '',
-              center: 'title',
-              right: ''
+            this.changeView("listMonth");
+            this.setOption("headerToolbar", {
+              left: "title",
+              center: "",
+              right: "",
             });
-            this.setOption('footerToolbar', {
-              left:'prev,today,next',
-              center:'',
-              right:'subscribeButton'
+            this.setOption("footerToolbar", {
+              left: "prev,today,next",
+              center: "",
+              right: "subscribeButton",
             });
           } else {
-            this.changeView('dayGridMonth');
-            this.setOption('headerToolbar', {
-              left: 'title',
-              center: 'prev,today,next',
-              right: 'subscribeButton'
+            this.changeView("dayGridMonth");
+            this.setOption("headerToolbar", {
+              left: "title",
+              center: "",
+              right: "prev,today,next",
             });
-            this.setOption('footerToolbar', {
-              left:'',
-              center:'',
-              right:'dayGridMonth,listMonth'
+            this.setOption("footerToolbar", {
+              left: "subscribeButton",
+              center: "",
+              right: "dayGridMonth,listMonth",
             });
           }
         }}
-        loading={function(loading) {
+        loading={function (loading) {
           if (!loading) {
             if (window.innerWidth < 767) {
-              this.changeView('listMonth');
-              this.setOption('headerToolbar', {
-                left: '',
-                center: 'title',
-                right: ''
+              this.changeView("listMonth");
+              this.setOption("headerToolbar", {
+                left: "title",
+                center: "",
+                right: "",
               });
-              this.setOption('footerToolbar', {
-                left:'prev,today,next',
-                center:'',
-                right:'subscribeButton'
+              this.setOption("footerToolbar", {
+                left: "prev,today,next",
+                center: "",
+                right: "subscribeButton",
               });
             } else {
-              this.changeView('dayGridMonth');
-              this.setOption('headerToolbar', {
-                left: 'title',
-                center: 'prev,today,next',
-                right: 'subscribeButton'
+              this.changeView("dayGridMonth");
+              this.setOption("headerToolbar", {
+                left: "title",
+                center: "",
+                right: "prev,today,next",
               });
-              this.setOption('footerToolbar', {
-                left:'',
-                center:'',
-                right:'dayGridMonth,listMonth'
+              this.setOption("footerToolbar", {
+                left: "subscribeButton",
+                center: "",
+                right: "dayGridMonth,listMonth",
               });
             }
           }
         }}
-        eventClick={function(info) {
+        eventClick={function (info) {
           info.jsEvent.preventDefault();
           setModalContent(info.event.extendedProps.description);
           setModalTitle(info.event.title);
           setModalLocation(info.event.extendedProps.location);
           setModalStartDate(info.event.start);
-          setModalStartDate(info.event.end);
+          setModalEndDate(info.event.end);
           setModalOpen(true);
         }}
       />
