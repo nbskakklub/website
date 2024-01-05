32e993c58c45851a762ea3d0a0bb6e20d7758660
Jonathan Bangert <jonathan@bangert.dk>
2024-01-05 22:51:52 +0100

    Disable optimizations of images in index preview

--- src/components/cms/CmsPreviewIndex.tsx
+++ src/components/cms/CmsPreviewIndex.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import CmsPreviewBase from "./CmsPreviewBase";
@@ -5,60 +6,69 @@ import CmsPreviewBase from "./CmsPreviewBase";
 export default function Page({ title, cards, subtitle, description, children }) {
     const split_title = title.split(" ");
     return (
-        <CmsPreviewBase hasBackground={true}>
-            <div className="container">
-            <div className="head">
-                {/* eslint-disable-next-line @next/next/no-img-element*/}
-                <img
-                    src={"/images/clearoff.webp"}
-                    alt="Nørrebro Skakklub"
-                    className="bg_img"
-                ></img>
-                <div className="title">
-                {split_title.map((title_part) => (
-                    <h1 key={title_part} className="fancy_font">
-                    {title_part}
-                    </h1>
-                ))}
-                </div>
-                <button className="fancy_font
+      <CmsPreviewBase hasBackground={true}>
+        <div className="container">
+          <div className="head">
+            <img
+              src={"/images/clearoff.webp"}
+              alt="Nørrebro Skakklub"
+              className="bg_img"
+            ></img>
+            <div className="title">
+              {split_title.map((title_part) => (
+                <h1 key={title_part} className="fancy_font">
+                  {title_part}
+                </h1>
+              ))}
+            </div>
+            <button
+              className="fancy_font
                     poly_effect
-                    see_more">Se mere</button>
-            </div>
-            <div className="more_index" id="main-content">
-                <div className="more_content">
-                    <h1>{subtitle}</h1>
-                    <p>{description}</p>
-                    <div className="cards">
-                        <div className="card">
-                            <div className="img-container">
-                            <Image className="img" width={10} height={10} alt={cards[0].title} src={cards[0].image} />
-                            <div className="shadow"></div>
-                        </div>
-                            <span>
-                            {cards[0].title && <strong>{cards[0].title}&nbsp;</strong>}
-                            {cards[0].description}...
-                            <a> se mere</a>
-                            </span>
-                        </div>
-                        <div className="card">
-                            <div className="img-container">
-                            <Image className="img" width={10} height={10} alt={cards[1].title} src={cards[1].image} />
-                            <div className="shadow"></div>
-                        </div>
-                            <span>
-                            {cards[1].title && <strong>{cards[1].title}&nbsp;</strong>}
-                            {cards[1].description}...
-                            <a> se mere</a>
-                            </span>
-                        </div>
-                    </div>
-                <div className="practical_information">
-                    {children}
+                    see_more"
+            >
+              Se mere
+            </button>
+          </div>
+          <div className="more_index" id="main-content">
+            <div className="more_content">
+              <h1>{subtitle}</h1>
+              <p>{description}</p>
+              <div className="cards">
+                <div className="card">
+                  <div className="img-container">
+                    <img
+                      className="img"
+                      alt={cards[0].title}
+                      src={cards[0].image}
+                    />
+                    <div className="shadow"></div>
+                  </div>
+                  <span>
+                    {cards[0].title && <strong>{cards[0].title}&nbsp;</strong>}
+                    {cards[0].description}...
+                    <a> se mere</a>
+                  </span>
                 </div>
+                <div className="card">
+                  <div className="img-container">
+                    <img
+                      className="img"
+                      alt={cards[1].title}
+                      src={cards[1].image}
+                    />
+                    <div className="shadow"></div>
+                  </div>
+                  <span>
+                    {cards[1].title && <strong>{cards[1].title}&nbsp;</strong>}
+                    {cards[1].description}...
+                    <a> se mere</a>
+                  </span>
                 </div>
+              </div>
+              <div className="practical_information">{children}</div>
             </div>
-            </div>
-        </CmsPreviewBase>
-    )
+          </div>
+        </div>
+      </CmsPreviewBase>
+    );
 }
