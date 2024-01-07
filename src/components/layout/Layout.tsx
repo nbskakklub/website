113dee9e206155f0633373312e65f28bdac9c780
Jonathan Bangert <jonathan@bangert.dk>
2024-01-07 16:04:10 +0100

    Fix some config

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,15 +5,6 @@ import { Metadata } from "next";
 
 import styles from "./Layout.module.scss";
 
-export const metadata: Metadata = {
-  themeColor: "#fff",
-  applicationName: "Nørrebro skakklub",
-  description: "Hjemmeside af Nørrebro skakklub",
-  manifest: "/site.webmanifest",
-  viewport: "width=device-width, initial-scale=1",
-  icons: ["/icon.png"],
-};
-
 type Props = {
   children: React.ReactNode;
 };
